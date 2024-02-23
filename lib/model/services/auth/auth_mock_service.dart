d63b66886b507809e9b9f9d60a67ecc57fa87bac
Leonardo Moreira <lmoreira597@gmail.com>
2024-02-23 20:09:49 -0300

    implementação do mock

--- lib/model/services/auth/auth_mock_service.dart
+++ lib/model/services/auth/auth_mock_service.dart
@@ -1,11 +1,18 @@
 import 'dart:io';
-
+import 'dart:math';
 import 'package:mocuba/model/models/user_model.dart';
 import 'package:mocuba/model/services/auth/auth_service.dart';
+import 'dart:async';
 
 class AuthMockService implements AuthService {
   static Map<String, UserModel> _users = {};
   static UserModel? _currentUser;
+  static MultiStreamController<UserModel?>? _controller;
+  static final _userStream = Stream<UserModel?>.multi((controller) {
+    _controller = controller;
+    _updateUser(null);
+  });
+
   @override
   // TODO: implement currentUser
   UserModel? get currentUser {
@@ -14,24 +21,39 @@ class AuthMockService implements AuthService {
 
   @override
   Future<void> login(String email, String password) async {
-    // TODO: implement login
-    throw UnimplementedError();
+    _updateUser(_users[email]);
   }
 
   @override
   Future<void> logout() async {
-    // TODO: implement logout
-    throw UnimplementedError();
+    _updateUser(null);
   }
 
   @override
   Future<void> signup(
-      String nome, String email, String password, File image) async {
-    // TODO: implement signup
-    throw UnimplementedError();
+    String name,
+    String email,
+    String password,
+    File image,
+  ) async {
+    final newUser = UserModel(
+      id: Random().nextDouble().toString(),
+      name: name,
+      email: email,
+      imageURL: image.path,
+    );
+    _users.putIfAbsent(email, () => newUser);
+    _updateUser(newUser);
   }
 
   @override
   // TODO: implement userChanges
-  Stream<UserModel?> get userChanges => throw UnimplementedError();
+  Stream<UserModel?> get userChanges {
+    return _userStream;
+  }
+
+  static void _updateUser(UserModel? user) {
+    _currentUser = user;
+    _controller?.add(_currentUser);
+  }
 }
